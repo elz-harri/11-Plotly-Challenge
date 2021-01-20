5c972cf9de6a7783c51b3b184601752cf3bbee97
Elizabeth Harrison <elizabethgrace.dot@gmail.com>
2021-01-20 13:33:52 -0500

    final files uploaded

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,107 @@
-//Use the D3 library to read in samples.json
-//Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+  // Grab a reference to the dropdown select element
+  let selector = d3.select("#selDataset");
+  // Use the list of sample names to populate the select options
+  d3.json("samples.json").then((data) => {
+    let names = data.names;
+    names.forEach((sample) => {
+    selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
 
-function getSampleData(id) {
-    d3.json("samples.json").then((data) => {
-        console.log(data);
+    // Use the first sample from the list to build the initial plots
+    let firstSample = names[0];
+    
+    chartMaker(firstSample);
+    metadata(firstSample);
+  });
 
-      let metadata = data.metadata;
-      // Filter the data by subject's id
-      let idfilter = metadata.filter(item => item.id == id);
-      
-      let result = resultArray[0];
-      
-      // Use d3 to select the panel with id of `#sample-metadata`
-      let PANEL = d3.select("#sample-metadata");
+function metadata(sample) {
+  d3.json("samples.json").then((data) => {
+        let metadata = data.metadata;
+        // Loop through array of objects then each object
+        let filtered = metadata.filter((item) => item.id == sample);
+        console.log(filtered[0]);
+        // Then, select the unordered list element by class name
+        let list = d3.select("#sample-metadata");
+        // First, clear out any existing data
+        list.html("");
+        // Display the sample metadata, i.e., an individual's demographic information.
+        // Append the id data to the dropdown menu
+        list.append("li").text(filtered[0].id);
+        list.append("li").text(filtered[0].ethnicity);
+        list.append("li").text(filtered[0].gender);
+        list.append("li").text(filtered[0].age);
+        list.append("li").text(filtered[0].location); 
+  });
+}
+
+function chartMaker(sample) {
+d3.json("samples.json").then((data) => {
+  let samples = data.samples;
+  let sample_id = samples.filter(item => item.id == sample);
+  let result = sample_id[0];
   
-      // Use `.html("") to clear any existing metadata
-      PANEL.html("");
+  let otu_ids = result.otu_ids;
+  let otu_labels = result.otu_labels;
+  let sample_values = result.sample_values;
+  let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+
+  // Build BarChart
+   
+  let barTrace = [
+  {
+      y: yticks,
+      x: sample_values.slice(0, 10).reverse(),
+      text: otu_labels.slice(0, 10).reverse(),
+      type: "bar",
+      orientation: "h",
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: "Electric"
+        }
+  }
+  ];
+
+  let barLayout = {
+  title: "Top 10 Bacteria Cultures Found",
+  margin: { t: 30, l: 150 }
+  };
+  Plotly.newPlot("bar", barTrace, barLayout);
+
+  //Bubble Chart
   
-      // Use `Object.entries` to add each key and value pair to the panel
-      // Hint: Inside the loop, you will need to use d3 to append new
-      // tags for each key-value in the metadata.
-      Object.entries(result).forEach(([key, value]) => {
-        PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-      });
+  let bubbleTrace = [
+  {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
+      marker: {
+      size: sample_values,
+      color: otu_ids,
+      colorscale: "Electric"
+      }
+  }
+];
+
+  let bubbleChartLayout = {
+    title: "Bacteria Cultures Per Sample",
+    margin: { t: 0 },
+    hovermode: "closest",
+    xaxis: { title: "OTU ID" },
+    margin: { t: 30}
+    };
+ 
+  Plotly.newPlot("bubble", bubbleTrace, bubbleChartLayout);
+  
+});
+}
+
+function optionChanged(newSample) {
+// Fetch new data each time a new sample is selected
+chartMaker(newSample);
+metadata(newSample);
+}
